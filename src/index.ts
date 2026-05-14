1ec6b55e836ac08f8d2bb1f5506b2443b09d01d0
qwuide <joakimuhrwing@gmail.com>
2026-05-14 15:23:57 +0200

    Checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export type { T, TIn } from './runtime/t.js';
-export { t } from './runtime/t.js';
 export {
   getDefaultLocale,
   getLocale,
   getLocales,
   setLocale,
 } from './locale/store.js';
+export type { T, TIn } from './runtime/t.js';
+export { t } from './runtime/t.js';
