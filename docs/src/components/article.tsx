ce96c3d5d8cd25b9375608941133ebd6d46501ee
qwuide <joakimuhrwing@gmail.com>
2026-05-17 15:49:19 +0200

    Checkpoint

--- docs/src/components/article.tsx
+++ docs/src/components/article.tsx
@@ -2,9 +2,8 @@ import type { BoxProps } from '#components/box';
 import type { Article as ArticleData } from '#lib/article';
 
 import { Box } from '#components/box';
+import { MarkdocRenderer } from '#components/markdoc-renderer';
 
-import { ArticleBody } from './article/body';
-import { ArticleHeader } from './article/header';
 import styles from './article.module.css';
 
 export interface ArticleProps extends BoxProps<'article'> {
@@ -20,11 +19,28 @@ export function Article(props: ArticleProps) {
       as="article"
       className={[styles.Article, className]}
     >
-      <ArticleHeader
-        description={article.description}
-        title={article.title}
-      />
-      <ArticleBody tree={article.tree} />
+      <Box
+        as="header"
+        className={styles.Header}
+      >
+        <Box
+          as="h1"
+          className={styles.TitleHeading}
+        >
+          {article.title}
+        </Box>
+        {article.description && (
+          <Box
+            as="p"
+            className={styles.DescriptionParagraph}
+          >
+            {article.description}
+          </Box>
+        )}
+      </Box>
+      <Box className={styles.Body}>
+        <MarkdocRenderer tree={article.tree} />
+      </Box>
     </Box>
   );
 }
