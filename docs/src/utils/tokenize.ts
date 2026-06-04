6615ffab50eb6cdb5452e3409e284a56ef29d6fb
qwuide <joakimuhrwing@gmail.com>
2026-06-04 17:54:30 +0200

    Checkpoint

--- docs/src/utils/tokenize.ts
+++ docs/src/utils/tokenize.ts
@@ -1011,23 +1011,33 @@ function markTaggedTemplates(tokens: Token[]) {
   }
 }
 
+const RAW_TEXT_TAGS = new Set(['<script', '<style']);
+const RAW_TEXT_CLOSE_TAGS = new Set(['</script', '</style']);
+
 function reclassifyJsxText(tokens: Token[]) {
   let depth = 0;
   let inText = false;
   let exprDepth = 0;
+  let rawTextDepth = 0;
 
   for (const token of tokens) {
     if (token.type === 'jsx-tag') {
       inText = false;
       if (token.value === '/>') {
         depth = Math.max(0, depth - 1);
-        if (depth > 0 && exprDepth === 0) {
+        if (depth > 0 && exprDepth === 0 && rawTextDepth === 0) {
           inText = true;
         }
       } else if (token.value.startsWith('</')) {
         depth = Math.max(0, depth - 1);
+        if (RAW_TEXT_CLOSE_TAGS.has(token.value)) {
+          rawTextDepth = Math.max(0, rawTextDepth - 1);
+        }
       } else {
         depth++;
+        if (RAW_TEXT_TAGS.has(token.value)) {
+          rawTextDepth++;
+        }
       }
       continue;
     }
@@ -1035,7 +1045,7 @@ function reclassifyJsxText(tokens: Token[]) {
     if (token.type === 'punct') {
       const value = token.value;
       if (value === '>') {
-        if (depth > 0 && exprDepth === 0) {
+        if (depth > 0 && exprDepth === 0 && rawTextDepth === 0) {
           inText = true;
         }
         continue;
@@ -1053,7 +1063,7 @@ function reclassifyJsxText(tokens: Token[]) {
       if (value === '}' && exprDepth > 0) {
         const wasOutermost = exprDepth === 1;
         exprDepth--;
-        if (exprDepth === 0 && depth > 0) {
+        if (exprDepth === 0 && depth > 0 && rawTextDepth === 0) {
           inText = true;
         }
         if (wasOutermost && depth > 0) {
