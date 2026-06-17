35ac3e88b83f129e90dd53abc0f11bec873f74d8
qwuide <joakimuhrwing@gmail.com>
2026-06-17 10:40:03 +0200

    Checkpoint

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,23 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    coverage: {
-      exclude: [
-        '**/*.astro',
-        '**/*.svelte',
-        '**/*.test-d.ts',
-        '**/bin.ts',
-        '**/fixture/**',
-        'packages/*/src/runtime.ts',
-        'packages/{doc-extractor,tsdown-config,typescript-config,vitest-config}/**',
-        'packages/{react,vue,svelte}/src/hmr-patch.ts',
-        'packages/vue/src/dev-version.ts',
-        'packages/svelte/src/dev-version.svelte.ts',
-      ],
-      include: [
-        'packages/*/src/**/*.{ts,tsx}',
-      ],
-    },
     projects: [
       'packages/*',
     ],
