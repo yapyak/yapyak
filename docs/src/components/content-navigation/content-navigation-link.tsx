71dc12409f0f0743ed8bc2f8115703e8d9e1f66a
qwuide <joakimuhrwing@gmail.com>
2026-07-03 15:11:21 +0200

    Checkpoint

--- docs/src/components/content-navigation/content-navigation-link.tsx
+++ docs/src/components/content-navigation/content-navigation-link.tsx
@@ -24,7 +24,7 @@ export function ContentNavigationLink(props: ContentNavigationLinkProps) {
   useEffect(() => {
     if (isActiveOnMountRef.current) {
       element.current?.scrollIntoView({
-        block: 'nearest',
+        block: 'center',
       });
     }
   }, []);
