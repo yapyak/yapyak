02aa09d41592de59f63620ccb6c4da3603fc7779
qwuide <joakimuhrwing@gmail.com>
2026-07-29 13:08:06 +0200

    Checkpoint

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,24 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      exclude: [
+        '**/*.{d,test-d}.ts',
+        '**/fixture/**',
+        '**/{index,internal,type}.ts',
+        'packages/{*-config,doc-compiler}/**',
+      ],
+      include: [
+        'packages/*/src/**/*.{ts,tsx}',
+      ],
+      provider: 'v8',
+      reporter: [
+        'text-summary',
+        'html',
+        'lcov',
+        'json-summary',
+      ],
+    },
     projects: [
       'packages/*',
       'docs',
