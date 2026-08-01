e7a4a28598881f5bae6f8eaf34716beb8cb795dd
qwuide <joakimuhrwing@gmail.com>
2026-08-01 08:14:10 +0200

    Checkpoint

--- packages/svelte/src/ambient.d.ts
+++ packages/svelte/src/ambient.d.ts
@@ -10,10 +10,6 @@ interface ImportMeta {
   };
 }
 
-declare function $state<T>(initial: T): T;
-declare function $derived<T>(expression: T): T;
-declare function $effect(fn: () => undefined | (() => void)): void;
-
 declare module '*.svelte' {
   import type { Component } from 'svelte';
 
